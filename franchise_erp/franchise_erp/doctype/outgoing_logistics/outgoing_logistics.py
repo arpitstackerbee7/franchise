3e91da823214edc57d738c0f3c8ce17cae951d85
Arpit Panchal <Arpit.panchal@stackerbee.com>
2026-02-18 18:52:17 +0530

    outgoing logistics type wise list fetch id showing

--- franchise_erp/franchise_erp/doctype/outgoing_logistics/outgoing_logistics.py
+++ franchise_erp/franchise_erp/doctype/outgoing_logistics/outgoing_logistics.py
@@ -7,46 +7,74 @@ from frappe.model.document import Document
 
 class OutgoingLogistics(Document):
 
-	def on_submit(self, method=None):
-		if not self.sales_invoice_no:
-			return
+	# def on_submit(self, method=None):
+	# 	if not self.sales_invoice_no:
+	# 		return
 
-		for row in self.sales_invoice_no:
-			if not row.sales_invoice:
-				continue
+	# 	for row in self.sales_invoice_no:
+	# 		if not row.sales_invoice:
+	# 			continue
 
-			# Do not overwrite if already linked
-			existing_ref = frappe.db.get_value(
-				"Sales Invoice",
-				row.sales_invoice,
-				"custom_outgoing_logistics_reference"
-			)
+	# 		# Do not overwrite if already linked
+	# 		existing_ref = frappe.db.get_value(
+	# 			"Sales Invoice",
+	# 			row.sales_invoice,
+	# 			"custom_outgoing_logistics_reference"
+	# 		)
 
-			if existing_ref:
-				continue
+	# 		if existing_ref:
+	# 			continue
 
-			frappe.db.set_value(
-				"Sales Invoice",
-				row.sales_invoice,
-				"custom_outgoing_logistics_reference",
-				self.name
-			)
+	# 		frappe.db.set_value(
+	# 			"Sales Invoice",
+	# 			row.sales_invoice,
+	# 			"custom_outgoing_logistics_reference",
+	# 			self.name
+	# 		)
 
-			frappe.db.set_value(
-				"Sales Invoice",
-				row.sales_invoice,
-				"custom_outgoing_logistics_no",
-				self.name
-			)
+	# 		frappe.db.set_value(
+	# 			"Sales Invoice",
+	# 			row.sales_invoice,
+	# 			"custom_outgoing_logistics_no",
+	# 			self.name
+	# 		)
 
 			
-			frappe.db.set_value(
-				"Sales Invoice",
-				row.sales_invoice,
-				"custom_document_nolr_no",
-				self.document_no
-			)
+	# 		frappe.db.set_value(
+	# 			"Sales Invoice",
+	# 			row.sales_invoice,
+	# 			"custom_document_nolr_no",
+	# 			self.document_no
+	# 		)
 
+
+	def on_submit(self):
+
+		if not self.references:
+			return
+
+		for row in self.references:
+
+			if not row.source_doctype or not row.source_name:
+				continue
+
+			if not frappe.db.exists(row.source_doctype, row.source_name):
+				continue
+
+			# fields exist karte hain?
+			meta = frappe.get_meta(row.source_doctype)
+			if not meta.has_field("custom_outgoing_logistics_reference"):
+				continue
+
+			frappe.db.set_value(
+				row.source_doctype,
+				row.source_name,
+				{
+					"custom_outgoing_logistics_reference": self.name,
+					"custom_outgoing_logistics_no":self.name,
+					"custom_document_nolr_no": self.document_no
+				}
+			)
 
 
 	def before_cancel(self, method=None):
