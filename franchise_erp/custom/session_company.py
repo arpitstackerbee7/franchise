8b3badfe80b554b36d91915beee039b575ef5b1c
manpreet.k <manpreet.k@stackerbee.com>
2026-03-16 12:16:21 +0530

    delivery note

--- franchise_erp/custom/session_company.py
+++ franchise_erp/custom/session_company.py
@@ -20,6 +20,8 @@
 #         frappe.logger().info(
 #             f"Session company auto-set from User master: {user_company} for {user}"
 #         )
+
+
 import frappe
 
 def set_session_company_from_user():
