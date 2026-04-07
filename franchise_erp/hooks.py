cccc61bab1d3a8b476cae688af8dd9bda5e489b7
Arpit Panchal <Arpit.panchal@stackerbee.com>
2026-04-07 09:08:25 +0530

    comment one more file for live

--- franchise_erp/hooks.py
+++ franchise_erp/hooks.py
@@ -268,9 +268,6 @@ app_include_js = [
     "/assets/franchise_erp/js/customer_quick_entry_patch.js",
     "/assets/franchise_erp/js/uploader_patch.js",
     "/assets/franchise_erp/js/barcode_scanner_override.js",
-    "/assets/franchise_erp/js/fast_barcode.js"
-
-
 ]
 
    
