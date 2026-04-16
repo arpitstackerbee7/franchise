9b303b1ae83c4b58c7909169bdc668a6340e76cf
Arpit Panchal <Arpit.panchal@stackerbee.com>
2026-04-16 18:29:38 +0530

    comment code uncomment

--- franchise_erp/franchise_erp/doctype/outgoing_logistics/outgoing_logistics.py
+++ franchise_erp/franchise_erp/doctype/outgoing_logistics/outgoing_logistics.py
@@ -111,7 +111,7 @@ class OutgoingLogistics(Document):
                 barcode_value = self.name
                 self.db_set("barcode_value", barcode_value)
         # ✅ WhatsApp PDF send call
-        # send_sales_invoice_pdf_from_outgoing_logistics(self)
+        send_sales_invoice_pdf_from_outgoing_logistics(self)
     # -----------------------------
     # BEFORE CANCEL
     # -----------------------------
