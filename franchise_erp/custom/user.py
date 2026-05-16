941b871f518d083320f8b45c8672403e8048daf9
Shiv Kumar R <shiva.k@stackerbee.com>
2026-05-16 11:30:23 +0530

    The User Interface Grid View Changes

--- franchise_erp/custom/user.py
+++ franchise_erp/custom/user.py
@@ -9,16 +9,16 @@ def validate(doc, method=None):
 
     viewer_doc = frappe.get_single("User Role Viewer")
 
-    for row in viewer_doc.table_vjxt:
+    # collect blocked roles
+    restricted_roles = [row.role for row in viewer_doc.table_vjxt if cint(row.check)]
 
-        if cint(row.check):
-
+    # find matches in current document
+    for row in doc.roles:
+        if row.role in restricted_roles:
             blocked_roles.append(row.role)
 
-    for row in doc.roles:
-
-        if row.role in blocked_roles:
-
-            frappe.throw(
-                _("Role <b>{0}</b> is restricted").format(row.role)
-            )
+    # throw once with all roles
+    if blocked_roles:
+        frappe.throw(
+            _("Roles Restricted: <b>{0}</b>").format(", ".join(blocked_roles))
+        )
