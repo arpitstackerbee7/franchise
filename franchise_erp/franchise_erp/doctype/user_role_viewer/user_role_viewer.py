f4a9331c5a1b0c22d9111811708d078b2af6c403
Shiv Kumar R <shiva.k@stackerbee.com>
2026-05-16 10:47:36 +0530

    The User Interface Changes

--- franchise_erp/franchise_erp/doctype/user_role_viewer/user_role_viewer.py
+++ franchise_erp/franchise_erp/doctype/user_role_viewer/user_role_viewer.py
@@ -1,5 +1,7 @@
 # Copyright (c) 2026, Franchise Erp and contributors
 # For license information, please see license.txt
+# user_role_viewer.py
+
 import frappe
 from frappe.model.document import Document
 
@@ -7,33 +9,20 @@ from frappe.model.document import Document
 class UserRoleViewer(Document):
     pass
 
+
 @frappe.whitelist()
 def get_logged_user_roles():
-
-    user = frappe.session.user
-
-    assigned_roles = frappe.get_roles(user)
-
-    hidden_roles = ["Guest", "All"]
 
     all_roles = frappe.get_all(
         "Role",
         fields=["name"],
+        filters={
+            "disabled": 0
+        },
         order_by="name asc"
     )
 
-    role_data = []
-
-    for role in all_roles:
-
-        if role.name not in hidden_roles:
-
-            role_data.append({
-                "role": role.name,
-                "checked": role.name in assigned_roles
-            })
-
     return {
-        "user": user,
-        "roles": role_data
+        "user": frappe.session.user,
+        "roles": all_roles
     }
