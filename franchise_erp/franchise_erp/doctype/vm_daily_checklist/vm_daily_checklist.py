8d5965a60316b62d0417d334307e47fbd6985380
Shiv Kumar R <shiva.k@stackerbee.com>
2026-05-19 20:21:21 +0530

    The Daily Checklist Changes

--- franchise_erp/franchise_erp/doctype/vm_daily_checklist/vm_daily_checklist.py
+++ franchise_erp/franchise_erp/doctype/vm_daily_checklist/vm_daily_checklist.py
@@ -1,9 +1,44 @@
 # Copyright (c) 2026, Franchise Erp and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class VMDailyChecklist(Document):
 	pass
+
+
+
+def get_permission_query_conditions(user):
+
+    if not user:
+        user = frappe.session.user
+
+    if "System Manager" in frappe.get_roles(user):
+        return ""
+
+    return f"""
+        (
+            `tabVM Daily Checklist`.user_id = {frappe.db.escape(user)}
+            OR
+            `tabVM Daily Checklist`.asmtl_user_id = {frappe.db.escape(user)}
+        )
+    """
+
+
+def has_permission(doc, user=None, permission_type=None):
+
+    if not user:
+        user = frappe.session.user
+
+    if "System Manager" in frappe.get_roles(user):
+        return True
+
+    if getattr(doc, "user_id", None) == user:
+        return True
+
+    if getattr(doc, "asmtl_user_id", None) == user:
+        return True
+
+    return False
