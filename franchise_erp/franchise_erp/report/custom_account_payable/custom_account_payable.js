6725be6481434eed27dd979155975a944ca3a446
manish <manish.k@stackerbee.com>
2026-06-12 12:29:42 +0530

    feat: add custom accounts payable and receivable reports with document-wise grouping, bill date sorting, and running balance column

---
+++ franchise_erp/franchise_erp/report/custom_account_payable/custom_account_payable.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Franchise Erp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Custom Account Payable"] = {
+	"filters": [
+
+	]
+};
