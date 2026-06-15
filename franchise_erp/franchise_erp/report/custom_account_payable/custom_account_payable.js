b28b1904bf330ab2c68cd0c894a2556784926151
manish <manish.k@stackerbee.com>
2026-06-15 16:40:25 +0530

    feat: custom account payable & receivable - remove unwanted columns, add party-wise grouping, voucher type subtotals, posting date ascending sort, running balance

--- franchise_erp/franchise_erp/report/custom_account_payable/custom_account_payable.js
+++ franchise_erp/franchise_erp/report/custom_account_payable/custom_account_payable.js
@@ -1,8 +1,86 @@
-// Copyright (c) 2026, Franchise Erp and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["Custom Account Payable"] = {
-	"filters": [
+    filters: [
+        {
+            fieldname: "company",
+            label: __("Company"),
+            fieldtype: "Link",
+            options: "Company",
+            default: frappe.defaults.get_user_default("Company"),
+            reqd: 1,
+        },
+        {
+            fieldname: "report_date",
+            label: __("As On Date"),
+            fieldtype: "Date",
+            default: frappe.datetime.get_today(),
+            reqd: 1,
+        },
+        {
+            fieldname: "ageing_based_on",
+            label: __("Ageing Based On"),
+            fieldtype: "Select",
+            options: "Due Date\nPosting Date\nSupplier Invoice Date",
+            default: "Due Date",
+        },
+        {
+            fieldname: "party_type",
+            label: __("Party Type"),
+            fieldtype: "Select",
+            options: "\nSupplier\nCustomer\nEmployee\nShareholder\nStudent",
+            default: "Supplier",
+        },
+        {
+            fieldname: "party",
+            label: __("Party"),
+            fieldtype: "MultiSelectList",
+            get_data: function(txt) {
+                if (!frappe.query_report.filters) return;
+                let party_type = frappe.query_report.get_filter_value("party_type");
+                if (!party_type) return;
+                return frappe.db.get_link_options(party_type, txt);
+            },
+        },
+        {
+            fieldname: "payment_terms_template",
+            label: __("Payment Terms Template"),
+            fieldtype: "Link",
+            options: "Payment Terms Template",
+        },
+        {
+            fieldname: "group_by_party",
+            label: __("Group By Party"),
+            fieldtype: "Check",
+            default: 0,
+        },
+        {
+            fieldname: "based_on_payment_terms",
+            label: __("Based On Payment Terms"),
+            fieldtype: "Check",
+            default: 0,
+        },
+        {
+            fieldname: "show_future_payments",
+            label: __("Show Future Payments"),
+            fieldtype: "Check",
+            default: 0,
+        },
+        {
+            fieldname: "cost_center",
+            label: __("Cost Center"),
+            fieldtype: "Link",
+            options: "Cost Center",
+        },
+    ],
 
-	]
+    formatter: function(value, row, column, data, default_formatter) {
+        value = default_formatter(value, row, column, data);
+
+        if (data && data.is_subtotal) {
+            value = `<b>${value || ""}</b>`;
+        } else if (data && data.is_group) {
+            value = `<span style="color:#6c757d;">${value || ""}</span>`;
+        }
+
+        return value;
+    },
 };
