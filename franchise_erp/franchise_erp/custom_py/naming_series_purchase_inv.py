5245c0d80d3f3c157352214ea37bd1246030c303
prasanth0987 <prasanth.g@stackerbee.com>
2026-06-19 15:29:47 +0530

    Purchase invoice id change if service item

---
+++ franchise_erp/franchise_erp/custom_py/naming_series_purchase_inv.py
@@ -0,0 +1,19 @@
+import frappe
+def set_naming_series(doc, method):
+    all_service = True
+
+    for d in doc.items:
+        item_group = frappe.db.get_value(
+            "Item",
+            d.item_code,
+            "item_group"
+        )
+
+        if item_group != "All Item Groups-Services":
+            all_service = False
+            break
+
+    if all_service:
+        doc.naming_series = "SPI/.FY./."
+    else:
+        doc.naming_series = "PI/.FY./."
