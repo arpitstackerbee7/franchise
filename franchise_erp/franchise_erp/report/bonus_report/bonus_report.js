ab59edb3b7e65ca07860a503b323d6dcdd558a04
manish <manish.k@stackerbee.com>
2026-07-28 14:43:02 +0530

    feat: add bonus entry doctype, bonus report and custom fields in salary structure

---
+++ franchise_erp/franchise_erp/report/bonus_report/bonus_report.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2026, Franchise Erp and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Bonus Report"] = {
+    "filters": [
+        {
+            "fieldname": "bonus_year",
+            "label": __("Bonus Year"),
+            "fieldtype": "Select",
+            "options": ["2025-2026", "2026-2027"],
+            "reqd": 1,
+            "default": "2026-2027"
+        },
+        {
+            "fieldname": "employee",
+            "label": __("Employee"),
+            "fieldtype": "Link",
+            "options": "Employee"
+        },
+        {
+            "fieldname": "salary_structure",
+            "label": __("Salary Structure"),
+            "fieldtype": "Link",
+            "options": "Salary Structure"
+        },
+        {
+            "fieldname": "department",
+            "label": __("Department"),
+            "fieldtype": "Link",
+            "options": "Department"
+        }
+    ]
+};
